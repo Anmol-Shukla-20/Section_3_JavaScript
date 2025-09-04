6efe56b2d85295e7654c3fb04cad804bd3c13b95
Anmol-Shukla-20 <shuklaanmol811@gmail.com>
2025-09-04 20:47:38 +0530

    Covers basics about datatypes, Indexing in JS.

---
+++ index.js
@@ -0,0 +1,23 @@
+console.log('hello javascript');
+console.log('Nice');
+
+a = 34;
+
+console.log(a);
+
+console.log(typeof (a));
+
+msg = 'Hello';
+console.log(msg);
+
+isHungry = true;
+
+console.log(isHungry);
+
+console.log(1 == 1);
+console.log(1 == '1');
+console.log(1 === '1');
+
+console.log(1 + '1');
+console.log(1 + 1 + '1');
+console.log(1 + '1' + 1);
