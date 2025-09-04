46b7563f92ab5ed1b3ef842f33595d1a599a0d50
Anmol-Shukla-20 <shuklaanmol811@gmail.com>
2025-09-04 20:57:54 +0530

    Covers fundamentals about conditions in JS.

---
+++ conditions.js
@@ -0,0 +1,35 @@
+num = 34;
+
+if (num % 2 === 0) {
+    let myvar = 34;
+    const pi = 3.14;
+    
+    // pi = 5.345;
+
+    console.log('even');
+} else {
+    console.log('odd');
+}
+
+// console.log(myvar);
+
+const marks = 80;
+
+if(marks >= 90 && marks <= 100 ){
+    console.log('Grade A');
+}else if(marks >= 70 && marks < 90){
+    console.log('Grade B');
+}else if(marks >= 40 && marks < 70){
+    console.log('Grade C');
+}else{
+    console.log('Fail');
+}
+
+// WAP to check a person is eligible for DL
+const age = 56;
+
+if(age >= 18){
+    console.log('eligible for DL');
+}else{
+    console.log('not eligible for DL');
+}
